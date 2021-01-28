859c609ae67544631c49feb4a2bd01e2b6096d52
Mikus <mikusmeikulis@gmail.com>
2021-01-29 00:58:48 +0200

    deploy

---
+++ precache-manifest.60f04c82f76417ece4f49466a16a5ff2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "391209b08f6033003fb3",
+    "url": "/vue-jobs/css/app.cecbd11d.css"
+  },
+  {
+    "revision": "caae3d80c5dcaa93b083",
+    "url": "/vue-jobs/css/chunk-vendors.b0b28440.css"
+  },
+  {
+    "revision": "fc568113ad72433c2e3802588369dd2a",
+    "url": "/vue-jobs/img/backgroundImg.fc568113.png"
+  },
+  {
+    "revision": "f4c76e6743a8c75513899dff969e5d25",
+    "url": "/vue-jobs/img/logo.f4c76e67.png"
+  },
+  {
+    "revision": "f9704905bee718cbabe84e30543fc20f",
+    "url": "/vue-jobs/index.html"
+  },
+  {
+    "revision": "391209b08f6033003fb3",
+    "url": "/vue-jobs/js/app.55d89159.js"
+  },
+  {
+    "revision": "caae3d80c5dcaa93b083",
+    "url": "/vue-jobs/js/chunk-vendors.08e14125.js"
+  },
+  {
+    "revision": "ce5ead8727231ca0ddb7a37fdbc037d1",
+    "url": "/vue-jobs/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-jobs/robots.txt"
+  }
+]);
